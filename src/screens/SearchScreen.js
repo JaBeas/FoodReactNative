63ff88a18f5150b48a824e47ea830f7d0ebe1f57
Jaikar Beasley <jaikarbeasley@gmail.com>
2021-08-05 11:28:03 -0700

    added navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function SearchScreen(props) {
+  return (
+    <View>
+      <Text>Text updated</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
