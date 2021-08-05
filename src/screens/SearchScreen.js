efdd73fbf5a0155774be425b70cc7f4f5b669a40
Jaikar Beasley <jaikarbeasley@gmail.com>
2021-08-05 15:57:50 -0700

    added search bar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 function SearchScreen(props) {
+  const [term, setTerm] = useState("");
+
   return (
     <View>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log("term submitted")}
+      />
       <Text>Text updated</Text>
     </View>
   );
